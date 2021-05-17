3e1f6086ddbb5500879147bd34868a8a2b53aeb5
PortilloDev <ipp_1981@hotmail.com>
2021-05-17 15:30:52 +0200

    unit 19

---
+++ app/Http/Livewire/LandingPage.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Subscriber;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Livewire\Component;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+
+class LandingPage extends Component
+{
+    
+    public $email;
+
+    protected $rules = [
+        'email' => 'required|email:filter|unique:subscribers,email',
+    ];
+
+
+    public function subscribe() {
+
+        $this->validate();
+        DB::transaction(function () {
+            $subscriber = Subscriber::create([
+                'email' => $this->email,
+            ]);
+             $notification = new VerifyEmail;
+
+             $notification::createUrlUsing(function($notifiable) {
+                return URL::temporarySignedRoute(
+                    'subscribers.verify',
+                    now()->addMinutes(30),
+                    [
+                        'subscriber' => $notifiable->getKey(),
+                    ]
+                );
+             });
+
+             $subscriber->notify($notification);
+        }, $deadLockRetries = 5); 
+            
+            $this->reset('email');
+       
+    }
+
+    public function render()
+    {
+        return view('livewire.landing-page');
+    }
+
+    
+}
